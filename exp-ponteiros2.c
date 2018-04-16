2b0e0df83d41f9b0b903c6926de215b0f26d3081
kerolayne <kerolaynesouza@outlook.com.br>
2018-04-16 17:50:55 -0300

    Create exp-ponteiros2.c

---
+++ exp-ponteiros2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main() {
+  int *p;
+  printf("%d\n", p);
+
+  printf("%d\n", *p);
+
+  // todo ponteiro deve ser inicializado!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  return 0;
+}
