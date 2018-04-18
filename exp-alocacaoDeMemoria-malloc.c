84175a502c119d6c8516fc50b93f895ee00539a0
kerolayne <kerolaynesouza@outlook.com.br>
2018-04-18 18:26:36 -0300

    Update exp-alocacaoDeMemoria-malloc.c

--- exp-alocacaoDeMemoria-malloc.c
+++ exp-alocacaoDeMemoria-malloc.c
@@ -7,10 +7,24 @@
 //a função retorna o endereço de memoria da area alocada, ou a constante null, que indica que não foi possivel fazer a alocação.
 // cabeçalho malloc: void *                   malloc(size_t n);
 //                    '->ponteiro sem tipo              '-> inteiro longo não sinalizado= não negativo 
+// a função sizeof calcula o tamanho em bytes de um tipo de dados ou variavel.
+/* ex:int n = sizeof(double);
+      printf("double ocupa %d\n", n);
+      ----------------------
+      printf("char ocupa %d\n", sizeof(char));
+      ----------------------
+      int v[5];
+      printf("v ocupa %d\n", sizeof(v));
+      ----------------------
+      int v[] = {2,4,16,12,20}
+      int n = sizeof(v) / sizeof(int);
+*/
+
+
 
 //alocando um int 
 int main(int argc, char const *argv[]) {
-    int *p = malloc(4);
+    int *p = malloc(sizeof(int);
     *p = 12;
     printf("%d\n", *p);
 
