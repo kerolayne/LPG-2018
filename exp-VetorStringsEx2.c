c9dd931766f42e0bf0248287c262955e0f7ba697
kerolayne <kerolaynesouza@outlook.com.br>
2018-05-09 18:17:59 -0300

    Update exp-VetorStringsEx2.c

--- exp-VetorStringsEx2.c
+++ exp-VetorStringsEx2.c
@@ -2,34 +2,15 @@
 #include <stdoi.h>
 #include <string.h>
 
-char *aloca_str(){
-  char buffer[100];
 
-  scanf("%s", buffer );
-  //fgets(buffer, 99, stdin);
-  //buffer[strlen(buffer) - 1] = \0;
+int main() {
 
-  char *p = malloc(sizeof(char) * (strlen(buffer) + 1)); // +1 --> \0
-
-  strcpy(p, buffer);
-
-  return p;
-}
-
-int main(int argc, char const *argv[]) {
-  char *v[5];
+  char *dias[] = {"segunda", "terça", "quarta", "quinta", "sexta"};
   int i;
 
   for (i = 0; i < 5; i++) {
-    printf("digite o %dº nome\n", i + 1);
-    v[i] = aloca_str();
+    printf("%s\n", v[i] );
   }
-
-  for (i = 0; i < 5; i++) {
-    printf("%i: %s\n", i, v[i] );
-  }
-  for (i = 0; i < 5; i++) {
-    free(v[i]);
-  }
+  
   return 0;
 }
