2d44cbb23c085e703a19295670636226bdb4f190
kerolayne <kerolaynesouza@outlook.com.br>
2018-05-09 17:32:23 -0300

    Update exp-stringsEalocaçãoDinamica.c

--- exp-stringsEalocaçãoDinamica.c
+++ exp-stringsEalocaçãoDinamica.c
@@ -1,1 +1,24 @@
+/* -vetor de char
+      char nome[20];
+   -ponteiros de char, posteriormente fazer alocação;
+      int n = 20
+      char = *p;
+      p = malloc(sizef(char) * n);
 
+*/
+
+#include <stdlib.h>
+#include <stdoi.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  char s1[10] = "joão";
+  //s1 = "joao"; Errou!!!!!!!!!!!!!!
+  strcpy(s1, "Maria");
+
+  char *s2 = "maria", s3;
+
+  scanf("%s", s1 );
+  scanf("%s", s3 ); //erro pois s3 não foi inicializado 
+  return 0;
+}
