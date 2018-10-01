6a6afe2786d093c662ab42611caca6b95fff3bdd
Guilherme <guilhermebianeck@gmail.com>
2018-10-01 17:14:02 -0300

    Update exp-nAPartirDeK.c

--- exp-nAPartirDeK.c
+++ exp-nAPartirDeK.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int eh_primo(int x){
+int n_primo(int x){
   int i;
-  for(i = 2; i <= x/2; i++){
-    if(x % i == 0 ){
+  for(i = 2; i <= x/2; i++){ 
+    if(x % i == 0 ){ //Verifica se o número é primo.
       return 0;
     }
   }
@@ -16,14 +16,14 @@ int main() {
   int k = 0;
   int cont = 0;
 
-  printf("valor de n:\n");
+  printf("valor de n: \n");
   scanf("%d", &n);
-  printf("valor de k:\n");
+  printf("valor de k: \n");
   scanf("%d", &k);
 
   while(cont < n){
 
-    if(eh_primo(k)){
+    if(n_primo(k)){
       printf("%d é primo.\n", k);
       cont ++;
     }
